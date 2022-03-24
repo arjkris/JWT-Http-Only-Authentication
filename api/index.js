fa9a46319dbde055895b4c7c9f3f04712d63b27e
Arjun Krishnakumar <arjunkrishnakumar96.mec@gmail.com>
2022-03-24 18:54:19 -0500

    Added Docker Compose

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const expressjwt = require('express-jwt')
 const cookieParser = require('cookie-parser')
 const app = express()
-app.use(cors({origin:"http://localhost:3000",credentials:true}))
+app.use(cors())
 
 
 app.use(express.json())
